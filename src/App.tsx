544f7b9505ec8b764d3db8af883574c31b6e0a86
evelyn-2022 <evelyn20220101@gmail.com>
2024-07-05 14:08:31 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl text-orange-300'>Vite + React</h1>
+      <p className='text-slate-700'>Hello</p>
+    </>
+  );
+}
+
+export default App;
