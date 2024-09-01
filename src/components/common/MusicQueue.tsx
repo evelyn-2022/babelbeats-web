4062ba573131328a04ab5ceeaab6c19e001d454c
evelyn-2022 <evelyn20220101@gmail.com>
2024-09-01 20:37:41 +0100

    fix: auto scroll to current playing item

--- src/components/common/MusicQueue.tsx
+++ src/components/common/MusicQueue.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { YouTubeVideo } from '../../types';
 import MusicItem from './MusicItem';
 import { usePlayQueue } from '../../context';
@@ -9,6 +9,7 @@ const MusicQueue: React.FC<{
   maxHeight: string;
 }> = ({ playQueue, maxHeight }) => {
   const {
+    autoplay,
     currentVideoIndex,
     setCurrentVideoIndex,
     addVideoToBottomOfQueue,
@@ -18,6 +19,9 @@ const MusicQueue: React.FC<{
   } = usePlayQueue();
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const currentItemRef = useRef<HTMLDivElement | null>(null);
+
+  // Fetch playlist items when scrolling to the bottom
   const handleFetchPlaylistItems = async () => {
     if (!playlistId || isLoading || nextPageToken === null) return;
 
@@ -25,12 +29,9 @@ const MusicQueue: React.FC<{
 
     try {
       const response = await fetchPlaylistItems(playlistId, nextPageToken);
-
       const { items, nextPageToken: token } = response;
 
       items.map((item: YouTubeVideo) => addVideoToBottomOfQueue(item));
-
-      // setPlaylistItems(prevItems => [...prevItems, ...mappedItems]);
       setNextPageToken(token || null);
     } catch (error) {
       console.error('Error fetching playlist items:', error);
@@ -39,13 +40,13 @@ const MusicQueue: React.FC<{
     }
   };
 
+  // Handle scroll event to fetch more items
   useEffect(() => {
     const handleScroll = () => {
       const ulElement = document.querySelector('#playlist-ul');
       if (!ulElement) return;
 
       const { scrollTop, scrollHeight, clientHeight } = ulElement;
-
       if (scrollHeight - scrollTop === clientHeight && !isLoading) {
         handleFetchPlaylistItems();
       }
@@ -63,11 +64,20 @@ const MusicQueue: React.FC<{
     };
   }, [nextPageToken, isLoading]);
 
+  useEffect(() => {
+    if (currentItemRef.current) {
+      currentItemRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  }, [currentVideoIndex, autoplay]);
+
   return (
     <div className='w-full flex flex-col gap-2'>
       <h2 className='font-bold text-2xl'>Now playing</h2>
       <div
-        className={`flex flex-col w-full  overflow-y-auto`}
+        className={`flex flex-col w-full overflow-y-auto`}
         style={{
           maxHeight: maxHeight,
         }}
@@ -77,6 +87,7 @@ const MusicQueue: React.FC<{
           <div
             className='border-b-[1px] last:border-b-0 border-customWhite/20'
             key={index}
+            ref={index === currentVideoIndex ? currentItemRef : null} // Set ref to the current item
           >
             <MusicItem
               key={index.toString()}
